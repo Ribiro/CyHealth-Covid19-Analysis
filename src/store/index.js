c08b93bc9e224a1e68450abc772d3f50e01c7e10
Ribiro <deno059861>
2022-11-20 01:25:16 +0300

    finalized on chart

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,16 @@ axios.defaults.baseURL = 'https://covid-193.p.rapidapi.com';
 axios.defaults.headers.common['X-RapidAPI-Key'] = '2de426e07amsh74f877917c143b7p1eb7f7jsn22096181602d';
 axios.defaults.headers.common['X-RapidAPI-Host'] = 'covid-193.p.rapidapi.com';
 
+
 export default new Vuex.Store({
     // state
     state: {
         statistics: [],
-        history: {},
+        countries: [],
+        labels: [],
+        cases_data: [],
+        deaths_data: [],
+        tests_data: []
     },
 
     // mutations
@@ -21,9 +26,16 @@ export default new Vuex.Store({
             state.statistics = payload
         },
 
-        // set the fetched history
-        setHistory(state, payload){
-            state.history = payload
+        setHistoryData(state, payload){
+            state.labels = payload.labels
+            state.cases_data = payload.cases
+            state.deaths_data = payload.deaths
+            state.tests_data = payload.tests
+        },
+
+        // set the fetched countries
+        setCountries(state, payload){
+            state.countries = payload
         }
     },
 
@@ -60,15 +72,23 @@ export default new Vuex.Store({
             })
         },
 
-        // get history
-        getHistory(context, country, day){   
-            axios.defaults.params = {country: country, day: day};         
+        // Fetch countries
+        getCountries(context){
             return new Promise((resolve, reject) => {
-                axios.get('/history')
+                axios.get('/countries')
                     .then(response => {
-                        context.commit('setHistory', response.data)
+                        const countries_data = response.data.response
+                        let countries = []
+
+                        for (let i=0; i < countries_data.length; i++){
+                            countries.push({
+                                id: i,
+                                country: countries_data[i]
+                            })
+                        }
+                        // console.log(countries)
+                        context.commit('setCountries', countries)
                         resolve(resolve)
-                        console.log(response)
                     })
                     .catch(error => {
                         console.log(error);
@@ -76,5 +96,70 @@ export default new Vuex.Store({
                     })
             })
         },
+
+        // get history
+        getHistory(context, payload){         
+            return new Promise((resolve, reject) => {
+                axios.get('/history', {params: {country: payload.country, day: payload.day}})
+                    .then(response => {
+                        const history = response.data.response;
+                        const historyData = [];
+                        const payload_data = {
+                            cases: [],
+                            deaths: [],
+                            tests: [],
+                            labels: []
+                        }
+
+                        history.forEach(function(item, index){
+                            var date = new Date(item.time);
+
+                            const date_to_use = date.getUTCHours() + ':' + date.getUTCMinutes() + ':' + date.getUTCSeconds()
+                            historyData.push({
+                                Index: index,
+                                New_Cases: item.cases.new*1,
+                                New_Deaths: item.deaths.new*1,
+                                Total_Tests: item.tests['1M_pop'],
+                                Time: item.time,
+                                UTC_Time: date,
+                                Formatted_Time: date_to_use
+                            })
+                        })
+
+                        for (let i =historyData.length-1; i>=0; i--){
+                            payload_data.cases.push(historyData[i].New_Cases)
+                            payload_data.deaths.push(historyData[i].New_Deaths)
+                            payload_data.tests.push(historyData[i].Total_Tests)
+                            payload_data.labels.push(historyData[i].Formatted_Time)
+                        }
+                        // console.log(payload_data)
+                        // context.commit('setHistory', historyData)
+                        context.commit('setHistoryData', payload_data)
+                        resolve(resolve)
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        reject(error)
+                    })
+            })
+        },
+
+        setHistoryData(){
+        }
+    },
+
+    getters: {
+        labels: state => {
+            return state.labels;
+        },
+        cases_data: state => {
+            return state.cases_data;
+        },
+        deaths_data: state => {
+            return state.deaths_data;
+        },
+        tests_data: state => {
+            return state.tests_data;
+        }
     }
 })
